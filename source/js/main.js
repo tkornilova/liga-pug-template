dad2f8cf054691780b189f79e4f90200b7f5be22
tkornilova <kornilova.tat07@gmail.com>
2023-05-17 13:05:46 +0400

    init: добавляет страницу test-4

--- source/js/main.js
+++ source/js/main.js
@@ -3,7 +3,7 @@ import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {CustomSelect} from './modules/select/custom-select';
 import {uploadFile, uploadImageDrop} from './modules/input-file/init-upload';
-import {getMapData, map1, map2, map3} from './modules/maps/init-maps';
+import {getMapData, map1, map2, map3, map4} from './modules/maps/init-maps';
 
 // ---------------------------------
 
@@ -14,6 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
   getMapData(map1);
   getMapData(map2);
   getMapData(map3);
+  getMapData(map4);
   iosVhFix();
 
 
